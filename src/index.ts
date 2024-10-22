50227094dbe11594e51f2b189e914b38abaecce7
Tiago de Oliveira <tiago@debian>
2024-10-22 18:24:19 -0300

    Start

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('HolaMundo');
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    console.log('Hola mundo');
+    res.send('Hola mundo');
+})
+
+app.listen(6505, () => {
+    console.log("Server activo");
+});
