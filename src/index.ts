97fb74c892c85480beeaa37d73bb8436d642908b
Tiago de Oliveira <tiago@debian>
2024-10-27 20:38:17 -0300

    feature: Implement caching with Redis

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import app from './app';
 import { AppDataSource } from './databases/connection';
+import { createClient } from 'redis';
+
+const client = createClient();
 
 async function main() {
     try {
         await AppDataSource.initialize();
         console.log('Database connected.');
+        await client.connect();
         app.listen(6505, () => {
             console.log("Server running.");
         });
     } catch (error) {
-        if(error instanceof Error){
+        if (error instanceof Error) {
             console.log(error.message);
         }
     }
 }
 
 main();
+export { client };
